0f712b9c059e4ae7309e302483a009fed001c641
Mandi Walls <mandi.walls@gmail.com>
2018-06-25 12:01:57 +0100

    adding workshop user and git package

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,17 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+#
+
+user 'chef' do
+  comment 'Workshop User'
+  home '/home/chef'
+  manage_home true
+  shell '/bin/bash'
+  password '$6$wWw6yhyC$hho7PULLzh.I7U9tg9anGX8etSCh9Nac24TBn1StZR9BNveYopRZNyhl29C3bOb7x332qlVR/HkX13yEWQmn//'
+  action :create
+end
+
+package 'git'
+
+
