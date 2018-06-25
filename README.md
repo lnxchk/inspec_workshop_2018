aeb0220866c5b443653f1ae6be9e002654633e8c
Mandi Walls <mandi.walls@gmail.com>
2018-06-25 11:57:46 +0100

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# inspec_workshop_2018
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: inspec_workshop_2018
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
